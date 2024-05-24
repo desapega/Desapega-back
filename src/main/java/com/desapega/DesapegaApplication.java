af0202984ec8bf43ae1f6f119b5200acf5c824d2
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-05-24 20:21:34 -0300

    first

---
+++ src/main/java/com/desapega/DesapegaApplication.java
@@ -0,0 +1,13 @@
+package com.desapega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesapegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesapegaApplication.class, args);
+	}
+
+}
