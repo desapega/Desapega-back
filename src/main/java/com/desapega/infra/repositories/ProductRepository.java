08adb98141c84c97c56883e4aded82eb7cd74ed6
GiselleBarbosa <g88barbosa@gmail.com>
2024-06-08 11:41:16 -0300

    feature/adicionado product repository

--- src/main/java/com/desapega/infra/repositories/ProductRepository.java
+++ src/main/java/com/desapega/infra/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.desapega.infra.repositories;
 
-public interface ProductRepository {
+import com.desapega.infra.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
